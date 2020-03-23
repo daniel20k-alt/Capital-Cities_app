049eb850f9d81621137d8e3379fb1e5e87d4a1c3
Testing_author <testing_beta@mail.com>
2020-03-23 22:00:45 +0200

    Initial Commit

---
+++ Capital Cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capital Cities
+//
+//  Created by DDDD on 23/03/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
